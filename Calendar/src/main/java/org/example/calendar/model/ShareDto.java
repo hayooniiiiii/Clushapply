caeba7fe564f7301a640580331562c582a907646
hayooniiiiii <140838571+hayooniiiiii@users.noreply.github.com>
2025-02-16 19:37:56 +0900

    editor

--- Calendar/src/main/java/org/example/calendar/model/ShareDto.java
+++ Calendar/src/main/java/org/example/calendar/model/ShareDto.java
@@ -1,7 +1,8 @@
 package org.example.calendar.model;
 
-
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -9,8 +10,9 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class ShareDto {
-    private Long id;
-    private Long diaryId;
-    private Long userId;
-
+    private Long shareId; // 공유 ID
+    private Long diaryId; // 공유된 다이어리 ID
+    private int userId; // 공유된 사용자 ID
+    private Boolean shareStatus; // 현재 편집 중인지 여부
+    private LocalDateTime shareTime; // 편집 시작 시간
 }
